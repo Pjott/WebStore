de714ea50d123ade38008e4cd6a7e21f89e1c1ee
Piotr Bala <piotr.baa@gmail.com>
2017-08-22 19:41:29 +0200

    First version of welcome page and home controller

---
+++ src/main/java/com/pjott/webstore/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.pjott.webstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		
+		model.addAttribute("welcomeText", "Welcome to my awsome Web Store !!!");
+		model.addAttribute("taglineText", "The best web store in the world !!!");
+		
+		return "welcome";
+	}
+}
