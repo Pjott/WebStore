f093ec264713505d5fd3bbb3577f58ca10eaa4b0
Piotr Bala <piotr.baa@gmail.com>
2017-08-24 20:36:56 +0200

    Scripts for Database

---
+++ src/main/resources/db/sql/insert-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRODUCTS VALUES ('A34C', 'Bumbulator', 'Bumbulator z dodatkową przystawką i kalfasakiem','500','Odidos','WTF','New',50,0,false);
+INSERT INTO PRODUCTS VALUES ('X775', 'Szybkozłączka','Fajna szybkozłączka, mosiądz',10,'SCO Inc.','Coupling','New',600,0,false);
+INSERT INTO PRODUCTS VALUES ('PN1002', 'Kołnierz', 'Kołnierz DN100 PN40, 316L',650,'FSS','Fitting','New',49,0,false);
