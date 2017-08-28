27f0966a3c3a296a2ea7930bc7230ec71ccefa0b
Piotr Bala <piotr.baa@gmail.com>
2017-08-28 22:44:41 +0200

    Created ProductController - adding list of products to the model

--- src/main/java/com/pjott/webstore/controller/ProductController.java
+++ src/main/java/com/pjott/webstore/controller/ProductController.java
@@ -1,25 +1,29 @@
 package com.pjott.webstore.controller;
 
-import java.math.BigDecimal;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.pjott.webstore.domain.Product;
+import com.pjott.webstore.domain.repository.ProductRepository;
 
 @Controller
 public class ProductController {
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@RequestMapping("/products")
 	public String list(Model model) {
 		
-		Product itemOne = new Product("A34C", "Bumbulator", new BigDecimal(505));
+	/*	Product itemOne = new Product("A34C", "Bumbulator", new BigDecimal(505));
 		itemOne.setDescription("Bumbulator z dodatkową przystawką i kalfasakiem");
 		itemOne.setCategory("WTF");
 		itemOne.setManufacturer("Odidos");
 		itemOne.setUnitsInStock(20);
-		model.addAttribute("product", itemOne);
+		model.addAttribute("product", itemOne); */
+		
+		model.addAttribute("products", productRepository.getAllProducts());
 		
 		return "productsPage";
 	}
