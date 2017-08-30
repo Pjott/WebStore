7b152655aa7689b0243428ccfbd14a4af808cc5c
Piotr Bala <piotr.baa@gmail.com>
2017-08-30 22:11:03 +0200

    Update Customer (change field type) - FIX

--- src/main/java/com/pjott/webstore/domain/Customer.java
+++ src/main/java/com/pjott/webstore/domain/Customer.java
@@ -5,13 +5,13 @@ public class Customer {
 	private String customerId;
 	private String name;
 	private String address;
-	private String noOfOrdersMade;
+	private int noOfOrdersMade;
 	
 	public Customer() {
 		super();
 	}
 
-	public Customer(String customerId, String name, String address, String noOfOrdersMade) {
+	public Customer(String customerId, String name, String address, int noOfOrdersMade) {
 		super();
 		this.customerId = customerId;
 		this.name = name;
@@ -43,14 +43,11 @@ public class Customer {
 		this.address = address;
 	}
 
-	public String getNoOfOrdersMade() {
+	public int getNoOfOrdersMade() {
 		return noOfOrdersMade;
 	}
 
-	public void setNoOfOrdersMade(String noOfOrdersMade) {
+	public void setNoOfOrdersMade(int noOfOrdersMade) {
 		this.noOfOrdersMade = noOfOrdersMade;
-	}
-	
-	
-	
+	}	
 }
