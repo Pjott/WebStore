bc7ecae30cb9a95578e9cc61e19610b9e9e06db3
Piotr Bala <piotr.baa@gmail.com>
2017-08-30 22:13:55 +0200

    Adding SQL scripts for creating customers table and inserting data into it

--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -1,3 +1,6 @@
 INSERT INTO PRODUCTS VALUES ('A34C', 'Bumbulator', 'Bumbulator z dodatkową przystawką i kalfasakiem','500','Odidos','WTF','New',50,0,false);
 INSERT INTO PRODUCTS VALUES ('X775', 'Szybkozłączka','Fajna szybkozłączka, mosiądz',10,'SCO Inc.','Coupling','New',600,0,false);
 INSERT INTO PRODUCTS VALUES ('PN1002', 'Kołnierz', 'Kołnierz DN100 PN40, 316L',650,'FSS','Fitting','New',49,0,false);
+INSERT INTO CUSTOMERS VALUES ('AA23', 'Jan Kowalski','Warszawa, ul. Klonowa 3',15);
+INSERT INTO CUSTOMERS VALUES ('AA24', 'Adam Nowak','Gdynia, ul. Korczaka 223',8);
+INSERT INTO CUSTOMERS VALUES ('AA25', 'Monika Kolano','Szczecin, ul. Bajkowa 4',3);
