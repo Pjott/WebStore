64d28c36902ecc821235ce83a552ebb8ee3305bd
Piotr Bala <piotr.baa@gmail.com>
2017-08-30 22:17:08 +0200

    Created a persistence layer for customers

---
+++ src/main/java/com/pjott/webstore/domain/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.pjott.webstore.domain.repository;
+
+import java.util.List;
+
+import com.pjott.webstore.domain.Customer;
+
+public interface CustomerRepository {
+
+	List<Customer> getAllCustomers();
+}
---
+++ src/main/java/com/pjott/webstore/domain/repository/impl/InMemoryCustomerRepository.java
@@ -0,0 +1,45 @@
+package com.pjott.webstore.domain.repository.impl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.pjott.webstore.domain.Customer;
+import com.pjott.webstore.domain.repository.CustomerRepository;
+
+@Repository
+public class InMemoryCustomerRepository implements CustomerRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplate;
+	
+	@Override
+	public List<Customer> getAllCustomers() {
+		Map<String, Object> customerParams = new HashMap<String, Object>();
+		List<Customer> customerResult = jdbcTemplate.query("SELECT * FROM customers", customerParams, new CustomerMapper());
+		
+		return customerResult;
+	}
+
+	private static final class CustomerMapper implements RowMapper<Customer> {
+
+		@Override
+		public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Customer customer = new Customer();
+			customer.setCustomerId(rs.getString("ID"));
+			customer.setName(rs.getString("NAME"));
+			customer.setAddress(rs.getString("ADDRESS"));
+			customer.setNoOfOrdersMade(rs.getInt("NOOFORDERSMADE"));
+			
+			return customer;
+		}
+	}
+	
+}
