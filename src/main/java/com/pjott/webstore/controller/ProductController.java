ba77c92ce16e1e430dce9a092d8db864f11c5595
Piotr Bala <piotr.baa@gmail.com>
2017-08-31 22:08:54 +0200

    Added getProductsByCategory in ProductController, new request mapping for ProductController and btw for HomeController

--- src/main/java/com/pjott/webstore/controller/ProductController.java
+++ src/main/java/com/pjott/webstore/controller/ProductController.java
@@ -3,12 +3,14 @@ package com.pjott.webstore.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pjott.webstore.domain.repository.ProductRepository;
 import com.pjott.webstore.service.ProductService;
 
 @Controller
+@RequestMapping("market")
 public class ProductController {
 	
 	@Autowired
@@ -25,6 +27,13 @@ public class ProductController {
 	public String updateSrock(Model model) {
 		productService.updateAllStock();
 		
-		return "redirect:/products";
+		return "redirect:/market/products";
+	}
+	
+	@RequestMapping("/products/{category}")
+	public String getProductsByCategory(Model model, @PathVariable("category") String productCategory) {
+		model.addAttribute("products", productService.getProductsByCategory(productCategory));
+		
+		return "productsPage";
 	}
 }
