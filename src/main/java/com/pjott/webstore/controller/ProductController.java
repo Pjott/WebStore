41a4893b43e8c97474f3825bd2f8caa042179a30
Piotr Bala <piotr.baa@gmail.com>
2017-09-04 20:16:41 +0200

    Added another request mapping (matrix variables). Searching(fiter) by categories(category) and brands(manufacturer)

--- src/main/java/com/pjott/webstore/controller/ProductController.java
+++ src/main/java/com/pjott/webstore/controller/ProductController.java
@@ -1,12 +1,15 @@
 package com.pjott.webstore.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.pjott.webstore.domain.repository.ProductRepository;
 import com.pjott.webstore.service.ProductService;
 
 @Controller
@@ -33,6 +36,12 @@ public class ProductController {
 	@RequestMapping("/products/{category}")
 	public String getProductsByCategory(Model model, @PathVariable("category") String productCategory) {
 		model.addAttribute("products", productService.getProductsByCategory(productCategory));
+		
+		return "productsPage";
+	}
+	@RequestMapping("/products/filter/{params}")
+	public String getProductsByFilter(Model model, @MatrixVariable(pathVar="params") Map<String, List<String>> filterParams) {
+		model.addAttribute("products", productService.getProductsByFilter(filterParams));
 		
 		return "productsPage";
 	}
