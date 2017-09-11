4f9379dbf58f291d44172b60f866b5c347e8ba5f
Piotr Bala <piotr.baa@gmail.com>
2017-09-11 21:14:29 +0200

    Updating DB script

--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -1,4 +1,5 @@
 INSERT INTO PRODUCTS VALUES ('A34C', 'Bumbulator', 'Bumbulator z dodatkową przystawką i kalfasakiem','500','Odidos','WTF','New',50,0,false);
+INSERT INTO PRODUCTS VALUES ('XD23', 'Bumbulator', 'Bumbulator z dodatkową przystawką', '150', 'Odidos', 'WTF', 'New', 100, 0 , false);
 INSERT INTO PRODUCTS VALUES ('X775', 'Szybkozłączka','Fajna szybkozłączka, mosiądz',10,'SCO Inc.','Coupling','New',600,0,false);
 INSERT INTO PRODUCTS VALUES ('PN1002', 'Kołnierz', 'Kołnierz DN100 PN40, 316L',650,'FSS','Fitting','New',49,0,false);
 INSERT INTO CUSTOMERS VALUES ('AA23', 'Jan Kowalski','Warszawa, ul. Klonowa 3',15);
