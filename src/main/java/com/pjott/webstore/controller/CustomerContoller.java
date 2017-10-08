5b90c67ca29f464489a61514c5a28f55fef1b90e
Piotr Bala <piotr.baa@gmail.com>
2017-10-08 18:14:58 +0200

    Update Customer controller - handling adding new customer

--- src/main/java/com/pjott/webstore/controller/CustomerContoller.java
+++ src/main/java/com/pjott/webstore/controller/CustomerContoller.java
@@ -3,8 +3,12 @@ package com.pjott.webstore.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.pjott.webstore.domain.Customer;
+import com.pjott.webstore.domain.Product;
 import com.pjott.webstore.service.CustomerService;
 
 @Controller
@@ -18,5 +22,19 @@ public class CustomerContoller {
 		model.addAttribute("customers", customerService.getAllCustomers());
 		
 		return "customersPage";
+	}
+	
+	@RequestMapping(value = "/customers/add", method = RequestMethod.GET)
+	public String getAddNewCustomerForm(Model model) {
+		Customer newCustomer = new Customer();
+		model.addAttribute("newCustomer", newCustomer);
+		return "addCustomerPage";
+	}
+	
+	@RequestMapping(value = "/customers/add", method = RequestMethod.POST)
+	public String processAddNewCustomerForm(@ModelAttribute("newCustomer") Customer newCustomer) {
+		customerService.addCustomer(newCustomer);
+		
+		return "redirect:/customers/add";
 	}
 }
