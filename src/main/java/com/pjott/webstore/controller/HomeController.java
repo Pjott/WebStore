9271fd86ff2de94c0a23ea83af14fdc3bdaffc0b
Piotr Bala <piotr.baa@gmail.com>
2017-10-10 17:47:41 +0200

    Adding Redirecting Attributes (Flash Attributes) in welcome page)

--- src/main/java/com/pjott/webstore/controller/HomeController.java
+++ src/main/java/com/pjott/webstore/controller/HomeController.java
@@ -3,17 +3,25 @@ package com.pjott.webstore.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 	
 	@RequestMapping
-	public String welcome(Model model) {
+	public String welcome(Model model, RedirectAttributes redirectAttributes) {
 		
 		model.addAttribute("welcomeText", "Welcome to my awsome Web Store !!!");
 		model.addAttribute("taglineText", "The best web store in the world !!!");
+		redirectAttributes.addFlashAttribute("welcomeText", "Welcome to my awsome Web Store !!!");
+		redirectAttributes.addFlashAttribute("taglineText", "The best web store in the world !!!");
 		
+		return "redirect:/welcome/greeting";
+	}
+	
+	@RequestMapping("/welcome/greeting")
+	public String greeting() {
 		return "welcomePage";
 	}
 }
